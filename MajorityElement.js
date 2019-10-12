05c6224328dfa887290cf12e81cf35576124e17c
shch8713 <47016159+shch8713@users.noreply.github.com>
2019-10-11 19:33:46 -0600

    Create MajorityElement.js

---
+++ MajorityElement.js
@@ -0,0 +1,21 @@
+/**
+ * Solution to LeetCode problem 169. Majority Element
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    var len = nums.length;
+    var maj = Math.floor(len/2)
+    for (i = 0; i < len; i++){
+        var check = nums[i];
+        var count = 0;
+        for (j = 0; j < len; j++){
+            if(nums[j] == nums[i]){
+                count ++;
+                if(count > maj)
+                    return check;
+            }
+        }
+    }
+    
+};
